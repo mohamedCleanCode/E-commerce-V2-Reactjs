e02f9dc0c3c22ab7cdabf9276993405bc3d9758a
mohamed <m635379@gmail.com>
2023-05-25 15:58:44 +0200

    end filterSide

--- src/components/FilterSide/FilterSide.jsx
+++ src/components/FilterSide/FilterSide.jsx
@@ -31,7 +31,7 @@ function FilterSide({ title, links }) {
       </Accordion>
       <Accordion className="border-top" defaultActiveKey="0">
         <Accordion.Item eventKey="0">
-          <Accordion.Header>Brands</Accordion.Header>
+          <Accordion.Header>Features</Accordion.Header>
           <Accordion.Body>
             <Form>
               <Form.Check
@@ -45,6 +45,32 @@ function FilterSide({ title, links }) {
                 id={`Super Power`}
               />
               <a href="/">See all</a>
+            </Form>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+      <Accordion className="border-top" defaultActiveKey="0">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Price Rang</Accordion.Header>
+          <Accordion.Body>
+            <Form>
+              from <input type="number" min="1" /> <br />
+              to <br />
+              <input type="number" min="1" />
+            </Form>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+      <Accordion className="border-top" defaultActiveKey="0">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Ratings</Accordion.Header>
+          <Accordion.Body>
+            <Form>
+              <Form.Check type="checkbox" label={`1 star`} id={`1 star`} />
+              <Form.Check type="checkbox" label={`2 star`} id={`2 star`} />
+              <Form.Check type="checkbox" label={`3 star`} id={`3 star`} />
+              <Form.Check type="checkbox" label={`4 star`} id={`4 star`} />
+              <Form.Check type="checkbox" label={`5 star`} id={`5 star`} />
             </Form>
           </Accordion.Body>
         </Accordion.Item>
