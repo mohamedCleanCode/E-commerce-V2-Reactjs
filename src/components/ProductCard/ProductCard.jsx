9e0c3d0a35aa55d1cd63423255f9adabd1761e29
mohamed <m635379@gmail.com>
2023-08-19 12:49:34 +0200

    add some features

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,6 +1,9 @@
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Card, Col } from "react-bootstrap";
 import ReactStars from "react-rating-stars-component";
+import { Link } from "react-router-dom";
 import oppo from "../../assets/images/08.png";
 import "./ProductCard.css";
 
@@ -10,9 +13,12 @@ const ProductCard = ({ product }) => {
       <Card className="w-100">
         <Card.Img variant="top" src={oppo} />
         <Card.Body>
-          <Card.Title>
-            $99.50 <del>$1128.00</del>
-          </Card.Title>
+          <div className="d-flex justify-content-between align-items-center">
+            <Card.Title>
+              $99.50 <del>$1128.00</del>
+            </Card.Title>
+            <FontAwesomeIcon icon={faHeart} />
+          </div>
           <Card.Text className="d-flex align-items-center">
             <ReactStars count={5} size={24} activeColor="#ffd700" value={3} />
             <span className="d-inline ms-2" style={{ color: "#ffd700" }}>
@@ -23,6 +29,7 @@ const ProductCard = ({ product }) => {
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </Card.Text>
+          <Link to="/">View Details</Link>
         </Card.Body>
       </Card>
     </Col>
